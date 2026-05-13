bfb870cf2180ee48212d803533b61223f3c417ed
Your Name <you@example.com>
2026-05-13 15:04:26 +0200

    changshhhd

--- src/models/prisma.js
+++ src/models/prisma.js
@@ -1,11 +1,19 @@
 import 'dotenv/config';
+
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from '@prisma/adapter-pg';
- 
-const adapter = new PrismaPg({
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+
+const { Pool } = pg;
+
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-const prisma = new PrismaClient({ adapter });
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({
+  adapter,
+});
 
 export default prisma;
